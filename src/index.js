1d503c4a33404d10de9ed7f5936c0345a3b933cf
Listopad02 <123ayetvbass@gmail.com>
2022-01-13 19:44:31 +0300

    project layout created

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
 class App extends React.Component {
+  state = {
+    starWarsChars: [
+      { name: 'Darth Wader', side: 'dark' },
+      { name: 'Luke Skywalker', side: 'light' },
+      { name: 'Darth Sidious', side: 'dark' },
+      { name: 'Obi-Wan Kenobi', side: 'light' }
+    ]
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <ul>
+        { this.state.starWarsChars.map((char, i) => {
+          return (
+            <li key={char.name + i}>
+              <strong>{char.name}</strong> - &nbsp;
+              { char.side }
+            </li>
+          )
+        }) }
+      </ul>
     )
   }
 }
